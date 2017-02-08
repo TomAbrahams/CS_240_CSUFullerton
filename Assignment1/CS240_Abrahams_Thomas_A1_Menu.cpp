439b7cfe0fad217807ed69f0812ac8a600cc1948
TomAbrahams <UTKeitarol@gmail.com>
2017-02-07 19:47:59 -0800

    Update CS240_Abrahams_Thomas_A1_Menu.cpp

--- Assignment1/CS240_Abrahams_Thomas_A1_Menu.cpp
+++ Assignment1/CS240_Abrahams_Thomas_A1_Menu.cpp
@@ -1,8 +1,8 @@
 /*
-  Abrahams Thomas
-  February 7, 2017
-  CS 240
-  TTh 2-3:50 PM
+Abrahams Thomas
+February 7, 2017
+CS 240 TTh 2-3:50 PM
+Assembly_Menu
 */
 
 #include <iostream>
@@ -16,30 +16,27 @@ int main()
 	cout << "Drinks....................$2" << endl;
 	cout << "Sandwiches................$4" << endl;
 
-	int drinks = 0;
-	int sandwiches = 0;
-	int total = 0;
+	short drinks = 0;
+	short sandwiches = 0;
+	short total = 0;
 	cout << "How many drinks? ";
 	cin >> drinks;
-	cout << endl;
 	cout << "How many sandwiches: ";
 	cin >> sandwiches;
 	
-
-
 	__asm 
 	{
 		//Drinks			// Destination, Item 
-		mov eax, drinks; // eax = drinks
-		add eax, drinks; // eax = 2*drinks
+		mov ax, drinks; // ax = drinks
+		add ax, drinks; // ax = 2*drinks
 		//Sandwiches
-		mov ebx, sandwiches; //ebx = sandwiches
-		add ebx, sandwiches; //ebx = 2*sandwiches
-		add ebx, sandwiches; //ebx = 3*sandwiches
-		add ebx, sandwiches; //ebx = 4*sandwiches
+		mov bx, sandwiches; //bx = sandwiches
+		add bx, sandwiches; //bx = 2*sandwiches
+		add bx, sandwiches; //bx = 3*sandwiches
+		add bx, sandwiches; //bx = 4*sandwiches
 		//Total
-		add eax, ebx; //eax = eax + ebx = 2*drinks + 4*sandwiches
-		mov total, eax;
+		add ax, bx; //ax = ax + bx = 2*drinks + 4*sandwiches
+		mov total, ax;
 	}
 	cout << "\tYour total bill $" << total << endl;
 	system("pause");
