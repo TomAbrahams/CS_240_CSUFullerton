661d549b5d64fa50d1e7687bc209aa03303db7d2
TomAbrahams <UTKeitarol@gmail.com>
2017-02-07 19:49:42 -0800

    Update CS240_Abrahams_Thomas_A1_Temp_Conv.cpp

--- Assignment1/CS240_Abrahams_Thomas_A1_Temp_Conv.cpp
+++ Assignment1/CS240_Abrahams_Thomas_A1_Temp_Conv.cpp
@@ -1,8 +1,8 @@
 /*
 Abrahams Thomas
 February 7, 2017
-CS 240
-TTh 2-3:50 PM
+CS 240 TTh 2-3:50 PM
+Assembly_Temp_Conv
 */
 #include <iostream>
 #include <cstdio>
@@ -10,19 +10,20 @@ TTh 2-3:50 PM
 using namespace std;
 
 int main() {
-	int tempFer = 0;
-	int tempCel = 0;
+	short tempFer = 0;
+	short tempCel = 0;
 	cout << "Enter temperature in Fahrenheit: ";
 	cin >> tempFer;
 
 	__asm {
-		mov eax, tempFer; //eax = tempFer
-		sub eax, 32; //eax = eax -32
-		imul eax, 5; //eax = 5*eax = 5*(eax-32)
-		sub edx, edx; //edx = 0
-		mov ebx, 9; //eax = (5/9)*(eax - 32)
-		idiv ebx; 
-		mov tempCel, eax; //tempCel = eax
+		mov ax, tempFer;	//ax = tempFer
+		sub ax, 32;			//ax = eax -32
+		mov cx, 5;			//cx = 5
+		imul ax, 5;			//ax = 5*ax = 5*(ax-32)
+		cwd					//Doubles bit
+		mov bx, 9;			//bx = 9
+		idiv bx;			//ax = ax / bx, dx = ax mod bx
+		mov tempCel, ax;	//tempCel = eax
 	}
 	int degreeSym = 248;
 	cout << tempFer << "F" << (char)degreeSym << " is " << tempCel << "C" << (char)degreeSym << endl;
